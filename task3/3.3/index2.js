8cfb3c4de75785c5fb48d008f8c3480032836d7d
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2024-11-12 23:55:07 +0300

    all mistakes correction 3.3

--- task3/3.3/index2.js
+++ task3/3.3/index2.js
@@ -1,6 +1,4 @@
 let tasks = prompt("Введіть будь-яке 5-ти значне Число", "12345");
-let task3 = parseInt(tasks);
-console.log(typeof task3);
 
 console.log(tasks[0]+ " " + tasks[1]+ " " + tasks[2]+ " " + tasks[3]+ " " + tasks[4]+ " ");
 alert(tasks[0]+ " " + tasks[1]+ " " + tasks[2]+ " " + tasks[3]+ " " + tasks[4]+ " ");
