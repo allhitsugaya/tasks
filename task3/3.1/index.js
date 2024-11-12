4860c00442f0b80024f5b3a6e35e5a3242b03cbb
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2024-11-12 23:51:47 +0300

    all mistakes correction

--- task3/3.1/index.js
+++ task3/3.1/index.js
@@ -5,7 +5,9 @@ console.log(typeof true);// Boolean
 console.log(typeof null );// Object
 console.log(typeof 10n );// BigInt
 console.log(typeof Symbol("id") );// Symbol
-console.log(typeof function(){} ); // function
+console.log(typeof function(){} ); // function not object in js just for me.
+let value = null;
+console.log(value === null ? "null" : typeof value);//null
 
 
 
