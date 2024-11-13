ae40d25f4f453eec1b4734dd37292efb6b107b82
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2024-11-13 23:07:20 +0300

    Удаление ненужных файлов

--- task3/3.2/index1.js
+++
@@ -1,5 +0,0 @@
-const a = prompt("Введіть значення", "Code is cool");
-const b = prompt("Введіть значення", "Java Script my beloved love!");
-const c = prompt("Введіть значення", "How interesting JavaScript!!");
-
-console.log( '\n' + a,'\n'+ b,'\n' + c);
