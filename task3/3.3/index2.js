fe264a46758b48f241be72b3e6855573f63b33de
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2024-11-13 23:02:54 +0300

    Додавання всех файлов

---
+++ task3/3.3/index2.js
@@ -0,0 +1,4 @@
+let tasks = prompt("Введіть будь-яке 5-ти значне Число", "12345");
+
+console.log(tasks[0]+ " " + tasks[1]+ " " + tasks[2]+ " " + tasks[3]+ " " + tasks[4]+ " ");
+alert(tasks[0]+ " " + tasks[1]+ " " + tasks[2]+ " " + tasks[3]+ " " + tasks[4]+ " ");
---
+++ task3/index.js
@@ -0,0 +1,12 @@
+console.log(typeof undefined );// undefined
+console.log(typeof 17 );// Number
+console.log(typeof "undefined" );// String
+console.log(typeof true);// Boolean
+console.log(typeof null );// Object
+console.log(typeof 10n );// BigInt
+console.log(typeof Symbol("id") );// Symbol
+console.log(typeof function(){} ); // function
+
+
+
+
