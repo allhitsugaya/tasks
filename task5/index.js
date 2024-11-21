113f05fd3cd1f49b059ace2216b8b72261dbd99a
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2024-11-21 17:58:01 +0200

    update task hw5.1

--- task5/index.js
+++ task5/index.js
@@ -1,7 +1,10 @@
 /*HW 5.1.*/
-for (let i = 20; i <= 30; i+=0.5) {
-console.log(i);
+let result = '';
+for (let i = 20; i <= 30; i += 0.5) {
+    result += i + ' ';
 }
+console.log(result.trim());
+
 
 /*HW 5.2.*/
 const dollar = 41
