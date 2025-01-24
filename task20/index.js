d23336891888011c0de3f383675ae85cbcc4b5c3
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2025-01-24 20:16:10 +0200

    updating task20

--- task20/index.js
+++ task20/index.js
@@ -2,29 +2,12 @@ function toDoList() {
     const valueinput = $('input').val().trim();
     if (valueinput === '') return;
 
-    const li = $('<li>', {
-        class: 'list-group-item d-flex align-items-center border-0 mb-2 rounded todolist'
-    });
-
-    const input = $('<input>', {
-        class: 'form-check-input me-2',
-        type: "checkbox"
-    });
-
-    const btnSee = $('<button>', {
-        class: 'lol',
-        text: 'See more'
-    });
-
-    const btnRemove = $('<button>', {
-        class: 'btn btn-danger',
-        text: 'Remove'
-    });
+    const li = $('<li>').addClass('list-group-item d-flex align-items-center border-0 mb-2 rounded todolist');
+    const input = $('<input>').addClass('form-check-input me-2').attr(`type`, `checkbox`);
+    const btnSee = $('<button>').addClass('lol').text(`See more`);
+    const btnRemove = $('<button>').addClass('btn btn-danger').text(`Remove`);
     $('.container').find('p').text(valueinput);
-    li.prepend(input);
-    li.append(valueinput);
-    li.append(btnSee);
-    li.append(btnRemove);
+    li.prepend(input).append(valueinput).append(btnSee).append(btnRemove);
     $('.list-group').append(li);
 }
 
@@ -41,16 +24,9 @@ $('.list-group').on('click', '.btn-danger', function () {
     $(this).closest('li').remove();
 });
 
-
 $('.list-group').on('change', '.form-check-input', function () {
-    const li = $(this).closest('li');
-    if ($(this).prop('checked')) {
-        $(li).css("text-decoration", "line-through");
-    } else {
-        $(li).css("text-decoration", "none");
-    }
+    const li = $(this).closest('li').prop('checked') ? $(this).css("text-decoration", "line-through") : $(this).css("text-decoration", "none");
 });
-
 
 $('#btn1, #closer').on('click', function () {
     $('.container').css('display', 'none');
@@ -58,7 +34,6 @@ $('#btn1, #closer').on('click', function () {
 
 $('.list-group').on('click', '.lol', function () {
     $('.container').css('display', 'block').css('opacity', '1');
-
 });
 
 
