f0d3e3d23d83811539f90a449e738534fa7ae443
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2025-02-12 09:58:54 +0200

    adding task25

---
+++ task25/my-task25/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ButtonShow from "./componets/ButtonShow.jsx";
+
+const App = () => {
+    return (
+        <>
+            <ButtonShow/>
+        </>
+
+    );
+};
+
+export default App;
+
+
+
